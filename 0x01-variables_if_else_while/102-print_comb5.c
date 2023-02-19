fc810bbef40be37ed6c6957d17f57632db2de69e
Kitesho-8499 <peter.kitesho@gmail.com>
2023-02-19 11:36:29 +0300

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -10,29 +10,29 @@ int main(void)
 {
 	int c = 0;
 	int f_d;
-	int l_d;
+	int p_d;
 
 	int c2;
 	int f_d2;
-	int 1_d2;
+	int r_d2;
 
 	while (c <= 98)
 	{
 		f_d = (c / 10 + '0');
-		1_d = (c % 10 + '0');
+		p_d = (c % 10 + '0');
 		c2 = 0;
 		while (c2 <= 99)
 		{
 			f_d2 = (c2 / 10 + '0');
-			1_d2 = (c2 % 10 + '0');
+			r_d2 = (c2 % 10 + '0');
 
 			if (c < c2)
 			{
 				putchar(f_d);
-				putchar(l_d);
+				putchar(p_d);
 				putchar(' ');
 				putchar(f_d2);
-				putchar(l_d2);
+				putchar(r_d2);
 
 				if (c != 98)
 				{
