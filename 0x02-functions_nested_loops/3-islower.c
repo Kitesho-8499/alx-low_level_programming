3e12f052cc0a23d343b8ee03de04a10c21dc4683
Kitesho-8499 <peter.kitesho@gmail.com>
2023-02-23 09:21:49 +0300

    3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _islower - furnction to prints lower case
+ * Return: 1 if it is a lower case
+ * and 0 otherwise
+ */
+
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+		return (1);
+	else
+		return (0);
+}
