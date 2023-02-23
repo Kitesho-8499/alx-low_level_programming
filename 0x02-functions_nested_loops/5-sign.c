e4726100c15e39fb109e4037f0b06a384cd5c242
Kitesho-8499 <peter.kitesho@gmail.com>
2023-02-23 13:39:42 +0300

    return (1);

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * print_sing - fuction that prints signs
+ * print_sign - fuction that prints signs
  *
  * @n: fuction parameter
  *
@@ -10,8 +10,9 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
+		else if (n == 0)
 		_putchar('+');
-		return (1);
 	}
-	else if (n == 0)
+	return (1);
+
 }
