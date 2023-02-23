708e6e03c89d362305047cb172aab818aed8f1fd
Kitesho-8499 <peter.kitesho@gmail.com>
2023-02-23 07:55:15 +0300

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
  * main - main fuction
- * _putchar: print the aphabets putchar
+ * _putchar: print the alphabets putchar
  * Return: Always 0
  */
 int main(void)
@@ -9,8 +9,8 @@ int main(void)
 	_putchar('_');
 	_putchar('p');
 	_putchar('u');
-	_putchar('t');
 	_putchar('c');
+	_putchar('');
 	_putchar('h');
 	_putchar('a');
 	_putchar('r');
