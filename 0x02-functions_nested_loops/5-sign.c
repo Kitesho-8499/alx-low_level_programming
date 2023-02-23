10906f1249a42394c7945434a3abca94f40f3a8a
Kitesho-8499 <peter.kitesho@gmail.com>
2023-02-23 09:49:38 +0300

    5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * print_sing - fuction that prints signs
+ *
+ * @n: fuction parameter
+ *
+ * Return: 1, 0 and -1
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('-');
+		_putchar(',');
+		_putchar(' ');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('-');
+                _putchar(',');
+                _putchar(' ');
+                return (0);
+	}
+	else
+	{
+		_putchar('-');
+                _putchar(',');
+                _putchar(' ');
+                return (-1);
+	}
+}
