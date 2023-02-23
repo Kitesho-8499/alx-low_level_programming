3da3c0d8502163141f715edb66092802c6731397
Kitesho-8499 <peter.kitesho@gmail.com>
2023-02-23 08:05:18 +0300

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,13 +7,13 @@
 int main(void)
 {
 	putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('c');
-	_putchar('t');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
+	putchar('p');
+	putchar('u');
+	putchar('c');
+	putchar('t');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
 	return (0);
 }
