c729f60bf9f98244c316f37962d58feb778fc0ee
Kitesho-8499 <peter.kitesho@gmail.com>
2023-02-27 15:14:58 -0800

    3-puts.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -3,11 +3,11 @@
  * _puts - Print a string followed by a new line to stdoutf
  * @str: string to print
  */
-void _puts(char * str)
+void _puts(char *)
 {
 	while (*str != '\0')
 	{
-		_puts(* str);
+		_puts(char  str);
 		str++;
 	}
 	_puts('\n');
