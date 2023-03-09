9cb769062c4d181381b2c7415992c75571ef7869
Kitesho-8499 <peter.kitesho@gmail.com>
2023-03-08 22:11:10 -0800

    0-puts_recursion.c

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _strlen_recursion - function that prints a string in reverse
+ * @s: the string to be print
+ * Return: length of string
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+		return (0);
+	s++;
+	return (1 + _strlen_recursion(s);
+}
