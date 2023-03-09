321e2f6bff53486d3efc40d157cffeaa41c3bda4
Kitesho-8499 <peter.kitesho@gmail.com>
2023-03-09 14:01:32 -0800

    ./2-strlen_recursion

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -6,8 +6,13 @@
  */
 int _strlen_recursion(char *s)
 {
-	if (*s == '\0')
-		return (0);
-	s++;
-	return (1 + _strlen_recursion(s);
+	int longit = 0;
+
+	if (*s)
+	{
+		longit++;
+		longit += _strlen_recursion(s + 1);
+	}
+	return (longit);
 }
+
