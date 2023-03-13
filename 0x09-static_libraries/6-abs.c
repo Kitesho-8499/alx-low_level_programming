5468bec461f3fc772c0c4e264edf7044405df175
Kitesho-8499 <peter.kitesho@gmail.com>
2023-03-13 16:15:46 -0700

    create_static_lib.sh

--- 0x09-static_libraries/6-abs.c
+++ 0x09-static_libraries/6-abs.c
@@ -2,17 +2,15 @@
 /**
  * _abs - check for absolute value
  *
- * @a: parameter to be checked
+ * @n: parameter to be checked
  *
  * Return: Always a
  */
-int _abs(int)
+int _abs(int n)
 {
-	int a;
-
-	if (a < 0)
-	a = -(a);|
-	else if (a >= 0)
-	a = a;
-	return (a);
+	if (n >= 0)
+	{
+		return (n);
+	}
+	return (-n);
 }
