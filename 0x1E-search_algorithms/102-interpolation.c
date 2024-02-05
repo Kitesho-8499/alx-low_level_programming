b19b3077c2a05b7bb4366b3e9e2bcd17175a7bca
Kitesho-8499 <peter.kitesho@gmail.com>
2024-02-05 04:04:57 -0800

    All tasks

---
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -0,0 +1,17 @@
+#include "search_algos.h"
+
+/**
+ * interpolation_search - searches for a value in an array of
+ * integers using the Interpolation search algorithm
+ *
+ * @array: input array
+ * @size: size of the array
+ * @value: value to search in
+ * Return: index of the number
+ */
+int interpolation_search(int *array, size_t size, int value)
+{
+	size_t pos, low, high;
+	double f;
+
+
